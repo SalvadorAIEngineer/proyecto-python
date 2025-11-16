498bf3fcf0062805f06fd1acb8e6b11f03d696dc
Salvador <workingsalvador25@gmail.com>
2025-11-16 10:28:14 -0600

    Programas del Dia 2: input, variables, suma y condicionales

---
+++ edad.py
@@ -0,0 +1,8 @@
+edad = int(input("Ingresa tu edad: "))
+
+if edad < 18:
+    print("Eres menor de edad")
+elif edad < 65:
+    print("Eres adulto")
+else:
+    print("Eres adulto mayor")
---
+++ suma.py
@@ -0,0 +1,6 @@
+a = int(input("Ingresa el primer número: "))
+b = int(input("Ingresa el segundo número: "))
+
+resultado = a + b
+
+print("La suma es:", resultado)
